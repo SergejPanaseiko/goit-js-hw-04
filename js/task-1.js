5630fcf39606832cfe365d43a1861290c4b88677
Sergey <konsaltpns@gmail.com>
2024-12-07 17:39:12 +0200

    add hw4

--- js/task-1.js
+++ js/task-1.js
@@ -1,13 +1,14 @@
-function slugify(title) {
-    const lowerCase=title.toLowerCase();
-    title = lowerCase.split(" ");
-    title = title.join("-");
-    return title;
+function isEnoughCapacity(products, containerSize) {
+    let total = 0;
+    let array = Object.values(products);
+    for (const variant of array)
+        total += variant;
+    return (total <= containerSize) ? true : false;
 }
 
-console.log("------------- TASK 1 -------------------");
-console.log(slugify("Arrays for beginners"));
-console.log(slugify("English for developer"));
-console.log(slugify("Ten secrets of JavaScript"));
-console.log(slugify("How to become a JUNIOR developer in TWO WEEKS"));
-console.log(" ");
+console.log(" ------------------ TASK 1 ------------------- ");
+console.log(isEnoughCapacity({ apples: 2, grapes: 3, carrots: 1 }, 8)); // true
+console.log(isEnoughCapacity({ apples: 4, grapes: 6, lime: 16 }, 12)); // false
+console.log(isEnoughCapacity({ apples: 1, lime: 5, tomatos: 3 }, 14)); // true
+console.log(isEnoughCapacity({ apples: 18, potatos: 5, oranges: 2 }, 7)); // false
+console.log("");
